e13c05c5ce8d73f87ca1574d3b226ce0885185dc
Matan Kotler-Berkowitz <205matan@gmail.com>
2023-08-05 23:42:42 +0200

    Add firebase initialization

---
+++ src/controllers/firebase.ts
@@ -0,0 +1,11 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: process.env.EXPO_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_FIREBASE_PROJECT_ID,
+  messagingSenderId: process.env.EXPO_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_FIREBASE_APP_ID,
+};
+
+export const app = initializeApp(firebaseConfig);
