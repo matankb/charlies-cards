5db06a894493a43dd5feee8338c3f68f6bcd24f0
Shai Mann-Robison <mann.robison@gmail.com>
2023-08-05 23:40:07 +0200

    Update settings.ts

--- src/controllers/settings.ts
+++ src/controllers/settings.ts
@@ -16,9 +16,6 @@ enum StorageKey {
     REFILL_LIMIT = "refill-limit",
 }
 
-/**
- * An interface for a credit card to be used internally by this application.
- */
 export interface CreditCard {
   cardNumber: string;
   cardHolder: string;
@@ -26,32 +23,18 @@ export interface CreditCard {
   cvv: string;
 }
 
-/**
- * @returns the {@link CreditCard} held in async storage.
- */
 export async function getCreditCard() {
     return AsyncStorage.getItem(StorageKey.CREDIT_CARD);
 }
 
-/**
- * Handles setting/updating of the credit card being used for charging automatic refills.
- * @param card the new {@link CreditCard} to set in async storage.
- */
 export async function setCreditCard(card: CreditCard) {
   return AsyncStorage.setItem(StorageKey.CREDIT_CARD, JSON.stringify(card));
 }
 
-/**
- * @returns the refill limit value held in async storage.
- */
 export async function getRefillLimit() {
     return AsyncStorage.getItem(StorageKey.REFILL_LIMIT);
 }
 
-/**
- * Handles setting/updating of the refill limit value.
- * @param limit the new threshold to store in async storage.
- */
 export async function setRefillLimit(limit: number) {
   return AsyncStorage.setItem(StorageKey.REFILL_LIMIT, limit.toString());
 }
