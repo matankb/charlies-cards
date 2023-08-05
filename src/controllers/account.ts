c3adc7b50e697e37750f90069f975ddc07f51384
Matan Kotler-Berkowitz <205matan@gmail.com>
2023-08-05 23:49:35 +0200

    Oops

--- src/controllers/account.ts
+++
@@ -1,8 +0,0 @@
-import { app, messaging } from './firebase';
-
-const MY_CHARLIE_USERNAME_KEY = 'MY_CHARLIE_USERNAME';
-const MY_CHARLIE_PASSWORD_KEY = 'MY_CHARLIE_PASSWORD';
-
-export function setMyCharlieCredentials(username: string, password: string) {
-  
-}
