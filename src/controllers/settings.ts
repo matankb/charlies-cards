84d0ed70e6a0ebef1f446bffd33e84dee01ea51b
Jacob Daitzman <jdtzmn@gmail.com>
2023-08-05 19:41:14 -0400

    Set up project (#5)
    * Add ESLint and Prettier
    
    * Add pre-commit hooks
    
    * Configure VSCode settings
    
    * Add GitHub CI Action
    
    * Format files with Prettier
    
    * Add `.eslintcache` to `.gitignore`

--- src/controllers/settings.ts
+++ src/controllers/settings.ts
@@ -1,38 +1,38 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 enum StorageKey {
-    /**
-     * The active credit card being used for billing during automatic refills.
-     */
-    CREDIT_CARD = "credit-card",
+  /**
+   * The active credit card being used for billing during automatic refills.
+   */
+  CREDIT_CARD = 'credit-card',
 
-    /**
-     * The targetted upper limit to attempt to refill the card to when refilling. When refilling, the smallest value that will take the
-     * Charlie Card over this value will be added. If a card is at $20 with a $40 limit, $25 will be added. The available values to add
-     * to a card online are $5, $10, $20, $25, $50.
-     */
-    REFILL_TARGET = "refill-target",
+  /**
+   * The targetted upper limit to attempt to refill the card to when refilling. When refilling, the smallest value that will take the
+   * Charlie Card over this value will be added. If a card is at $20 with a $40 limit, $25 will be added. The available values to add
+   * to a card online are $5, $10, $20, $25, $50.
+   */
+  REFILL_TARGET = 'refill-target',
 }
 
 export interface CreditCard {
-  cardNumber: string;
-  cardHolder: string;
-  expiration: string;
-  cvv: string;
+  cardNumber: string
+  cardHolder: string
+  expiration: string
+  cvv: string
 }
 
 export async function getCreditCard() {
-    return AsyncStorage.getItem(StorageKey.CREDIT_CARD);
+  return AsyncStorage.getItem(StorageKey.CREDIT_CARD)
 }
 
 export async function setCreditCard(card: CreditCard) {
-  return AsyncStorage.setItem(StorageKey.CREDIT_CARD, JSON.stringify(card));
+  return AsyncStorage.setItem(StorageKey.CREDIT_CARD, JSON.stringify(card))
 }
 
 export async function getRefillLimit() {
-    return AsyncStorage.getItem(StorageKey.REFILL_TARGET);
+  return AsyncStorage.getItem(StorageKey.REFILL_TARGET)
 }
 
 export async function setRefillLimit(limit: number) {
-  return AsyncStorage.setItem(StorageKey.REFILL_TARGET, limit.toString());
+  return AsyncStorage.setItem(StorageKey.REFILL_TARGET, limit.toString())
 }
