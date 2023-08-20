3824f192f94351e4f4002756350b6e8ecda8e77c
Shai Mann-Robison <mann.robison@gmail.com>
2023-08-20 15:49:08 -0400

    add button press style

--- src/components/PrimaryButton.tsx
+++ src/components/PrimaryButton.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { Pressable, Text } from 'react-native'
 
 interface SubmitButtonProps {
@@ -7,8 +7,16 @@ interface SubmitButtonProps {
 }
 
 export const PrimaryButton: FC<SubmitButtonProps> = ({ onSubmit, text }) => {
+  const [pressed, setPressed] = useState(false)
+
   return (
-    <Pressable className="w-full bg-blue rounded-lg p-2" onPress={onSubmit}>
+    <Pressable
+      className="w-full rounded-lg p-2"
+      onPress={onSubmit}
+      onPressIn={() => setPressed(true)}
+      onPressOut={() => setPressed(false)}
+      style={{ backgroundColor: pressed ? '#124B7A' : '#155C96' }}
+    >
       <Text
         className="text-white text-center"
         style={{ color: 'white', fontFamily: 'LatoBold', fontSize: 22 }}
