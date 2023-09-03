d03dba174344f03a0e6a133f0d1694aa1a385e75
Shai Mann-Robison <mann.robison@gmail.com>
2023-09-03 14:09:44 -0400

    remove dotenv dep

--- src/controllers/firebase.ts
+++ src/controllers/firebase.ts
@@ -1,12 +1,11 @@
 import { initializeApp } from 'firebase/app'
-import {} from '@env' // loads the module
 
 const firebaseConfig = {
-  apiKey: process.env.EXPO_FIREBASE_API_KEY,
-  authDomain: process.env.EXPO_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.EXPO_FIREBASE_PROJECT_ID,
-  messagingSenderId: process.env.EXPO_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_FIREBASE_APP_ID,
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 }
 
 export const app = initializeApp(firebaseConfig)
