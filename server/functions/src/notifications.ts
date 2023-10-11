e04e83978292cdf4baff7f3e1018895fd4c5d0b1
Matan Kotler-Berkowitz <205matan@gmail.com>
2023-10-11 16:25:17 -0400

    christ

--- server/functions/src/notifications.ts
+++ server/functions/src/notifications.ts
@@ -1,47 +1,45 @@
-import * as logger from "firebase-functions/logger";
-import { Expo } from "expo-server-sdk";
+import * as logger from 'firebase-functions/logger'
+import { Expo } from 'expo-server-sdk'
 
-import { User } from "./user";
+import { User } from './user'
 
-const expo = new Expo();
+const expo = new Expo()
 
 export async function sendNotifications(users: User[]) {
-  const pushTokens = users.map(user => user.notificationToken);
+  const pushTokens = users.map((user) => user.notificationToken)
 
-  const messages = [];
+  const messages = []
 
   for (const pushToken of pushTokens) {
     if (!Expo.isExpoPushToken(pushToken)) {
-      logger.error(`Push token ${pushToken} is not a valid Expo push token`);
-      continue;
+      logger.error(`Push token ${pushToken} is not a valid Expo push token`)
+      continue
     }
 
     messages.push({
       to: pushToken,
-      title: "Your MBTA card is running low!",
-      body: "Tap here to refill your card.",
-    });
+      title: 'Your MBTA card is running low!',
+      body: 'Tap here to refill your card.',
+    })
   }
 
   // Expo allows batching notifications
-  const chunks = expo.chunkPushNotifications(messages);
+  const chunks = expo.chunkPushNotifications(messages)
 
   for (const chunk of chunks) {
     try {
       // Send the chunks sequentially, instead of in parallel, to avoid rate limiting
-      const notificationResponses = await expo.sendPushNotificationsAsync(
-        chunk
-      );
+      const notificationResponses = await expo.sendPushNotificationsAsync(chunk)
 
       for (const response of notificationResponses) {
-        if (response.status === "error") {
+        if (response.status === 'error') {
           logger.error(
-            `There was an error sending a notification: ${response.details?.error}`
-          );
+            `There was an error sending a notification: ${response.details?.error}`,
+          )
         }
       }
     } catch (error) {
-      logger.error(error);
+      logger.error(error)
     }
   }
 }
--- src/controllers/notifications.ts
+++ src/controllers/notifications.ts
@@ -1,6 +1,6 @@
-import * as Device from "expo-device";
-import * as Notifications from "expo-notifications";
-import Constants from "expo-constants";
+import * as Device from 'expo-device'
+import * as Notifications from 'expo-notifications'
+import Constants from 'expo-constants'
 
 /**
  * Requests permission and registers the device for push notifications
@@ -9,25 +9,25 @@ import Constants from "expo-constants";
 export async function getNotificationToken() {
   // cannot register for push notifications in a simulator
   if (!Device.isDevice) {
-    return null;
+    return null
   }
 
   // request permission to send notifications
-  const { status: existingStatus } = await Notifications.getPermissionsAsync();
-  let finalStatus = existingStatus;
+  const { status: existingStatus } = await Notifications.getPermissionsAsync()
+  let finalStatus = existingStatus
 
-  if (existingStatus !== "granted") {
-    const { status } = await Notifications.requestPermissionsAsync();
-    finalStatus = status;
+  if (existingStatus !== 'granted') {
+    const { status } = await Notifications.requestPermissionsAsync()
+    finalStatus = status
   }
 
-  if (finalStatus !== "granted") {
-    return null;
+  if (finalStatus !== 'granted') {
+    return null
   }
 
   const token = await Notifications.getExpoPushTokenAsync({
     projectId: Constants.expoConfig.extra.eas.projectId,
-  });
+  })
 
-  return token.data;
+  return token.data
 }
